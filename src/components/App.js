9fff3efa982f7373dbb5b90d23ff5b393f8bec02
wangyishi <wangyishistl@gmail.com>
2019-01-05 23:52:26 -0600

    ShotChart

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import {TopBar} from "./TopBar"
+import {Main} from "./Main"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
+        <Main/>
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ShotChart} from "./ShortChart"
+
+export class Main extends React.Component {
+  render () {
+    return (
+      <div>
+        <ShotChart playerId={201939}/>
+      </div>
+    );
+  }
+}
