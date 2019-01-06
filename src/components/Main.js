24ed4dbd322d3d61efff315587b811d392cb408a
wangyishi <wangyishistl@gmail.com>
2019-01-06 01:08:45 -0600

    Profile image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {ShotChart} from "./ShortChart"
-import {Profile} from "./Profile"
+import {ShotChart} from "./ShortChart";
+import {Profile} from "./Profile";
+import nba from 'nba';
 
 export class Main extends React.Component {
+
+  state = {
+    playerId: nba.findPlayer('LeBron James').playerId,
+    playerInfo: {},
+  }
+
+  componentDidMount() {
+    nba.stats.playerInfo({PlayerID: this.state.playerId})
+      .then((info) => {
+        const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+        console.log(playerInfo, 'playerInfo');
+        this.setState({playerInfo});
+      })
+      .catch((e) => console.log(e))
+  }
+
   render () {
     return (
       <div className="main">
-        <Profile/>
-        <ShotChart playerId={201939}/>
+        <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
+        <ShotChart playerId={this.state.playerId}/>
       </div>
     );
   }
