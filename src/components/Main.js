dc35c51269134201c26a461717b14f1f2f0e15af
wangyishi <wangyishistl@gmail.com>
2019-01-06 00:06:12 -0600

    Profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ShotChart} from "./ShortChart"
+import {Profile} from "./Profile"
 
 export class Main extends React.Component {
   render () {
     return (
-      <div>
+      <div className="main">
+        <Profile/>
         <ShotChart playerId={201939}/>
       </div>
     );
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Profile extends React.Component {
+  render () {
+    return (
+      <div className="profile">
+        profile
+      </div>
+    );
+  }
+}
