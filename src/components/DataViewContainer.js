ab4d79672e269156fbe4de4f7f9bc0bd60de9413
wangyishi <wangyishistl@gmail.com>
2019-01-07 00:07:18 -0600

    debounce

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {ShotChart} from "./ShortChart";
 import {CountSlider} from "./CountSlider";
 import {Row, Col} from 'antd';
+import _ from 'lodash';
 
 export class DataViewContainer extends React.Component {
 
@@ -21,7 +22,8 @@ export class DataViewContainer extends React.Component {
           <Row className="filter-row">
             <Col span={2} offset={3} className="filter-label">Shots: </Col>
             <Col span={16}>
-              <CountSlider className="filter-control" onMinCountChange={this.onMinCountChange}/></Col>
+              <CountSlider className="filter-control" onMinCountChange={_.debounce(this.onMinCountChange, 500)}/>
+            </Col>
           </Row>
         </div>
       </div>
